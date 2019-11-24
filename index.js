013b4b8ec8f1ea40cffa390f711abd3574ee9269
Brian Bamsch <bbamsch@google.com>
2019-11-23 17:00:56 -0800

    Add validations during registration response
    Validations should really happen server-side. But this demo does not yet have a server-side. Hence we just do this in JavaScript for now (bad practice).

--- index.js
+++ index.js
@@ -22,20 +22,45 @@ function register() {
   logToTable(`App ID: <${appId}>`);
   const version = "U2F_V2";
   logToTable(`Version: <${version}>`);
-  const challenge = q['c'] !== undefined ? q['c'] : randomstring.generate(32);
-  const challengeEncoded = base64url.encode(challenge);
-  logToTable(`Challenge: <${challengeEncoded}>`);
+  const challenge = base64url.encode(q['c'] !== undefined ? q['c'] : randomstring.generate(32));
+  logToTable(`Challenge: <${challenge}>`);
   const timeout = q['t'] !== undefined ? parseInt(q['t']) : 60;
   logToTable(`Timeout: <${timeout}>`);
 
   const registerRequest = {
     appId: appId,
     version: version,
-    challenge: challengeEncoded,
+    challenge: challenge,
+  };
+
+  const validateRegistrationResponse = (registrationResponse) => {
+    return new Promise((resolve, reject) => {
+      /** These checks should probably happen server-side. But we don't have a server backend yet. */
+      if (registrationResponse.appId != appId) {
+        reject(new Error(`App ID Validation Failed: Expected <${appId}> but got <${registrationResponse.appId}>`));
+        return;
+      }
+
+      if (registrationResponse.challenge != challenge) {
+        reject(new Error(`Challenge Validation Failed: Expected <${challenge}> but got <${registrationResponse.challenge}>`));
+        return;
+      }
+
+      const clientData = JSON.parse(base64url.decode(registrationResponse.clientData));
+      if (clientData.challenge !== challenge) {
+        reject(new Error(`Challenge Validation Failed: Expected <${challenge}> but got <${clientData.challenge}>`));
+        return;
+      }
+      if (clientData.origin !== appId) {
+        reject(new Error(`Origin Validation Failed: Expected <${location.origin}> but got <${clientData.origin}>`));
+        return;
+      }
+      resolve(registrationResponse);
+    }).then(handleU2fRegistrationSuccessful);
   };
 
   u2f.register(registerRequest, timeout)
-    .then(handleU2fRegistrationSuccessful, handleU2fRegistrationUnsuccessful);
+    .then(validateRegistrationResponse, handleU2fRegistrationUnsuccessful)
 }
 
 
@@ -45,34 +70,42 @@ function handleU2fUnsupported() {
 
 
 function handleU2fRegistrationSuccessful(registrationResponse) {
-  const clientData = base64url.decode(registrationResponse.clientData);
+  console.log(registrationResponse);
+  const clientData = JSON.parse(base64url.decode(registrationResponse.clientData));
   const registrationData = base64url.decode(registrationResponse.registrationData);
+  // TODO: Send these to a server for storage / verification.
   logToTable(`Client Data: <${clientData}>`);
   logToTable(`Registration Data: <${registrationData}>`);
 }
 
 
 function handleU2fRegistrationUnsuccessful(error) {
-  const metadata = error.metaData;
-  switch (metadata.code) {
-    case u2f.ErrorCodes.BAD_REQUEST:
-      logToTable(`Invalid U2F Request`);
-      break;
-    case u2f.ErrorCodes.CONFIGURATION_UNSUPPORTED:
-      logToTable(`Unsupported U2F Configuration`);
-      break;
-    case u2f.ErrorCodes.DEVICE_INELIGIBLE:
-      logToTable(`Unsupported U2F Device`);
-      break;
-    case u2f.ErrorCodes.TIMEOUT:
-        logToTable(`Timed Out waiting for U2F Device Touch`);
+  if (error.metaData !== undefined) {
+    const metadata = error.metaData;
+    switch (metadata.code) {
+      case u2f.ErrorCodes.BAD_REQUEST:
+        logToTable(`Invalid U2F Request`);
         break;
-    case u2f.ErrorCodes.OK:
-    case u2f.ErrorCodes.OTHER_ERROR:
-    default:
-      logToTable(`Encountered unexpected error <${metadata.type}> during U2F`);
-      break;
+      case u2f.ErrorCodes.CONFIGURATION_UNSUPPORTED:
+        logToTable(`Unsupported U2F Configuration`);
+        break;
+      case u2f.ErrorCodes.DEVICE_INELIGIBLE:
+        logToTable(`Unsupported U2F Device`);
+        break;
+      case u2f.ErrorCodes.TIMEOUT:
+          logToTable(`Timed Out waiting for U2F Device Touch`);
+          break;
+      case u2f.ErrorCodes.OK:
+      case u2f.ErrorCodes.OTHER_ERROR:
+      default:
+        logToTable(`Encountered unexpected error <${metadata.type}> during U2F`);
+        break;
+    }
+    return;
   }
+
+  logToTable(error);
+  return;
 }
 
 /** Log to HTML Table */
